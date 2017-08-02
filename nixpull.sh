773ce51b38ea58620640b019237e1a4628575ab8
Michael Incardona <michael.incardona@outlook.com>
2017-08-02 09:34:40 -0400

    Remove unused vimacs setup commands

--- nixpull.sh
+++ nixpull.sh
@@ -13,13 +13,6 @@ mkdir --parents ~/.local/share/gtksourceview-3.0/styles
 cp --preserve "jcd.xml" ~/.local/share/gtksourceview-3.0/styles/
 cp --preserve ./templates/* ~/Templates/
 
-#unzip -o vimacs-master.zip
-#mkdir -p $HOME/.vim/doc
-#mkdir -p $HOME/.vim/plugin
-#cp -R vimacs-master/doc vimacs-master/plugin $HOME/.vim
-#vim --cmd "helptags $HOME/.vim/doc" --cmd "q"
-#rm -rf vimacs-master
-
 mkdir -p ~/.config/sublime-text-3/Packages/User/
 mkdir -p ~/.config/sublime-text-3/Packages/Custom\ Color\ Schemes/
 cp --preserve "Preferences.sublime-settings" ~/.config/sublime-text-3/Packages/User/

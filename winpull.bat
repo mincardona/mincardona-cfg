ca5c144eaedf6f0391420117ed3fdbdae8fb3119
Michael Incardona <michael.incardona@outlook.com>
2018-01-29 21:39:57 -0500

    Suppress some XCOPY output

--- winpull.bat
+++ winpull.bat
@@ -1,21 +1,21 @@
 @echo off
 
 REM Emacs
-XCOPY /Y emacs\.emacs "%APPDATA%\.emacs"
+XCOPY /Y /Q emacs\.emacs "%APPDATA%\.emacs"
 
 REM nano
-XCOPY /Y nano\.nanorc "%USERPROFILE%\.nanorc"
+XCOPY /Y /Q nano\.nanorc "%USERPROFILE%\.nanorc"
 
 REM Vim
-XCOPY /Y vim\.vimrc "%USERPROFILE%\.vimrc"
+XCOPY /Y /Q vim\.vimrc "%USERPROFILE%\.vimrc"
 
 REM bash
-XCOPY /Y bash\.bashrc "%USERPROFILE%\.bashrc"
-XCOPY /Y bash\.bash_aliases "%USERPROFILE%\.bash_aliases"
+XCOPY /Y /Q bash\.bashrc "%USERPROFILE%\.bashrc"
+XCOPY /Y /Q bash\.bash_aliases "%USERPROFILE%\.bash_aliases"
 
 REM Sublime Text 3
 MKDIR "%APPDATA%\Sublime Text 3\Packages\Custom Color Schemes\"
 MKDIR "%APPDATA%\Sublime Text 3\Packages\User\"
-XCOPY /Y sublime\Preferences.sublime-settings "%APPDATA%\Sublime Text 3\Packages\User\"
-XCOPY /Y sublime\Default.sublime-keymap "%APPDATA%\Sublime Text 3\Packages\User\"
-XCOPY /Y sublime\jcd.tmTheme "%APPDATA%\Sublime Text 3\Packages\Custom Color Schemes\"
+XCOPY /Y /Q sublime\Preferences.sublime-settings "%APPDATA%\Sublime Text 3\Packages\User\"
+XCOPY /Y /Q sublime\Default.sublime-keymap "%APPDATA%\Sublime Text 3\Packages\User\"
+XCOPY /Y /Q sublime\jcd.tmTheme "%APPDATA%\Sublime Text 3\Packages\Custom Color Schemes\"

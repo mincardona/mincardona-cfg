19b867fbc89ff43c6cf51728e0d3f5f06f098c7e
Michael Incardona <michael.incardona@outlook.com>
2021-09-08 21:12:08 -0400

    Change --preserve and --parents to -p
    For BSD compatibility

--- nixpull.sh
+++ nixpull.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # bash
-cp --preserve "bash/.bashrc" ~
-cp --preserve "bash/.bash_aliases" ~
+cp -p "bash/.bashrc" ~
+cp -p "bash/.bash_aliases" ~
 
 # zsh
-cp --preserve "zsh/.zshrc" ~
+cp -p "zsh/.zshrc" ~
 
 # vim
-cp --preserve "vim/.vimrc" ~
+cp -p "vim/.vimrc" ~
 mkdir -p ~/.vim/colors
 cp vim/.vim/colors/* ~/.vim/colors/
 mkdir -p ~/.vim/ftplugin
@@ -19,38 +19,38 @@ mkdir -p ~/.vim/doc
 cp vim/.vim/doc/* ~/.vim/doc/
 
 # nano
-cp --preserve "nano/.nanorc" ~
+cp -p "nano/.nanorc" ~
 
 # emacs
 mkdir -p ~/.emacs.d/lisp/
 rm -f ~/.emacs.d/lisp/ws-butler.el
-cp --preserve "emacs/.emacs" ~
+cp -p "emacs/.emacs" ~
 
 # gtksourceview
-mkdir --parents ~/.local/share/gtksourceview-3.0/styles
-cp --preserve "gtksourceview/jcd.xml" ~/.local/share/gtksourceview-3.0/styles/
+mkdir -p ~/.local/share/gtksourceview-3.0/styles
+cp -p "gtksourceview/jcd.xml" ~/.local/share/gtksourceview-3.0/styles/
 
 # templates
 if [ -d "~/Templates" ]; then
-    cp --preserve ./templates/* ~/Templates/
+    cp -p ./templates/* ~/Templates/
 fi
 
 # sublime
 mkdir -p ~/.config/sublime-text-3/Packages/User/
 mkdir -p ~/.config/sublime-text-3/Packages/Custom\ Color\ Schemes/
-cp --preserve "sublime/Preferences.sublime-settings" ~/.config/sublime-text-3/Packages/User/
-cp --preserve "sublime/Default.sublime-keymap" ~/.config/sublime-text-3/Packages/User/
-cp --preserve "sublime/Package Control.sublime-settings" ~/.config/sublime-text-3/Packages/User/
-cp --preserve "sublime/jcd.tmTheme" ~/.config/sublime-text-3/Packages/Custom\ Color\ Schemes/
+cp -p "sublime/Preferences.sublime-settings" ~/.config/sublime-text-3/Packages/User/
+cp -p "sublime/Default.sublime-keymap" ~/.config/sublime-text-3/Packages/User/
+cp -p "sublime/Package Control.sublime-settings" ~/.config/sublime-text-3/Packages/User/
+cp -p "sublime/jcd.tmTheme" ~/.config/sublime-text-3/Packages/Custom\ Color\ Schemes/
 
 # vscode - now using settings sync instead
 # mkdir -p ~/.config/Code/User
-# cp --preserve "vscode/settings.json" ~/.config/Code/User/
-# cp --preserve "vscode/keybindings.json" ~/.config/Code/User/
+# cp -p "vscode/settings.json" ~/.config/Code/User/
+# cp -p "vscode/keybindings.json" ~/.config/Code/User/
 # mkdir -p ~/.vscode/extensions
-# cp --preserve -r "vscode/extensions/michaelincardona.maclipse" ~/.vscode/extensions/
+# cp -p -r "vscode/extensions/michaelincardona.maclipse" ~/.vscode/extensions/
 
 # tmux
-cp --preserve "tmux/.tmux.conf" ~
+cp -p "tmux/.tmux.conf" ~
 
 echo "done."
